c0990343d847bfc1e29914638e2367e9bc4b25d5
Ford K Yang <yk804031@126.com>
2025-04-12 23:31:38 +0800

    LIGHTBLUE-0000: get data from prisma postgres db

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,13 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product-actions";
 
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const Homepage = async () => {
-  await delay(2000);
+  const latestProducts = await getLatestProducts();
   return (
     <>
-      <ProductList data={sampleData.products} title="Newest Arrivals" />
+      <ProductList data={latestProducts} title="Newest Arrivals" />
     </>
   );
 };
