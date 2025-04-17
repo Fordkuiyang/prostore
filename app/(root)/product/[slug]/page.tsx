28c13d81348c541945738e4f62a775095da0631e
Ford K Yang <yk804031@126.com>
2025-04-17 09:15:34 +0800

    LIGHTBLUE-0000: fix vercel deployment issue

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,7 +4,6 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product-actions";
-import { Divide } from "lucide-react";
 import { notFound } from "next/navigation";
 
 const ProductDetailPage = async (props: {
